86e1f9ec9527f318e1d2a19d562cbadd3f1a9998
Axel Minet <axel@cambridge105.fm>
2011-02-11 17:05:50 +0000

    updated version number in gigx-slides.php and updated features in readme

--- gigx-slides.php
+++ gigx-slides.php
@@ -4,7 +4,7 @@ Plugin Name: GIGX Slides
 Plugin URI: http://gigx.co.uk/wordpress/plugins/gigx-slides/
 Description: A Rotating Gallery Widget using a custom post type to create GIGX Slides.
 Author: AxWax
-Version: 0.0.2
+Version: 0.0.3
 Author URI: http://axwax.de/
 Credits:
 This plugin is based on Post Gallery Widget by Ron Rennick 
